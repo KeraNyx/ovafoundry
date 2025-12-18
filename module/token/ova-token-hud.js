910229d681e9657ef9729d10b31edf00b0131594
Kera <kat@sbih.org>
2025-12-17 20:05:10 -0800

    got it to show the freaking sheet

--- module/token/ova-token-hud.js
+++ module/token/ova-token-hud.js
@@ -1,21 +1,28 @@
-export default class OVATokenHUD extends foundry.appv1.hud.TokenHUD {
-    static get defaultOptions() {
-        return foundry.utils.mergeObject(super.defaultOptions, {
-            id: "token-hud",
-            template: "systems/ova/templates/token/token-hud.html"
-        });
+export default class OVATokenHUD extends TokenHUD {
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      id: "token-hud",
+      template: "systems/ova/templates/token/token-hud.html"
+    });
+  }
+
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    html
+      .querySelector('[data-action="trigger-effects"]')
+      ?.addEventListener("click", this._triggerActiveEffects.bind(this));
+  }
+
+  _triggerActiveEffects(event) {
+    event.preventDefault();
+
+    const targets = canvas.tokens.controlled
+      .map(t => t.actor)
+      .filter(Boolean);
+
+    for (const actor of targets) {
+      actor.triggerOverTimeEffects?.();
     }
-
-    activateListeners(html) {
-        super.activateListeners(html);
-
-        html.find('[data-action="trigger-effects"]').click(this._triggerActiveEffects.bind(this));
-    }
-
-    _triggerActiveEffects(event) {
-        event.preventDefault();
-
-        const targets = canvas.tokens.controlled.map(t => t.actor).filter(a => a);
-        targets.forEach(actor => actor.triggerOverTimeEffects?.());
-    }
+  }
 }
